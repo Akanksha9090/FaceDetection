f2b05e9ec6212a161ad7799fcb1ba1c5c9e4035c
Akanksha <akanksha600singh@gmail.com>
2021-09-20 19:16:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import face_recognition
+
+imgElon =face_recognition.load_image_file('C:\Users\Akanksha\Desktop\Elon1.jpg')
+imgElon=cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest =face_recognition.load_image_file('C:\Users\Akanksha\Desktop\Elon2.jpg')
+imgTest=cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+cv2.imshow('Elon Musk',imgElon)
+cv2.imshow('Elon Test',imgTest)
+cv2.waitKey(0)
